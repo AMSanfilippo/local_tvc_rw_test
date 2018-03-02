c58fb6e6a49582314c672b117318216ab4a9cdcf
AMSanfilippo <annamariesanfilippo@gmail.com>
2018-03-01 22:56:07 -0500

    fix gauss kernel and residual bootstrap

--- residual_bootstrap.py
+++ residual_bootstrap.py
@@ -6,33 +6,34 @@ Created on Tue Feb 27 12:28:54 2018
 @author: as
 """
 
+import numpy as np
+import pandas as pd
+
 # obtain fitted values, residuals, and centered residuals from estimated model
 # hypothesis: string, "null" or "alternative" (as of now this only handles alternative)
 # X: independent variable matrix with column of 1s for intercept
 # Y: dependent variable matrix
 # coeffs: dataframe of fitted tvcs from estimation of alternative (tvc) model
 # factor_model: string, either 'CAPM' or 'FF3'
-# start: row index of first time value for which we have coeff. estimates (integer)
-# end: row index of last time value for which we have coeff. estimates (integer)
-def get_residuals(hypothesis,X,Y,coeffs,factor_model,start,end):
+def get_residuals(hypothesis,X,Y,coeffs,factor_model):
     # constant component of fitted values
     fit_const = coeffs['const'].values
     # market return component of fitted values
-    fit_rmrf = [a*b for a,b in zip(X[start:end,1].flatten().tolist()[0],coeffs['rm_rf'].values)] 
+    fit_rmrf = [a*b for a,b in zip(X[:,1].flatten().tolist()[0],coeffs['rm_rf'].values)] 
     # lag-one return component of fitted values
-    fit_r_1 = [a*b for a,b in zip(X[start:end,-1].flatten().tolist()[0],coeffs['r_1'].values)]
+    fit_r_1 = [a*b for a,b in zip(X[:,-1].flatten().tolist()[0],coeffs['r_1'].values)]
 
-    fit_smb = [0]*len(X[start:end])
-    fit_hml = [0]*len(X[start:end])
+    fit_smb = [0]*len(X)
+    fit_hml = [0]*len(X)
 
     if factor_model == 'FF3':
-        fit_smb = [a*b for a,b in zip(X[start:end,2].flatten().tolist()[0],coeffs['smb'].values)]
-        fit_hml = [a*b for a,b in zip(X[start:end,3].flatten().tolist()[0],coeffs['hml'].values)]
+        fit_smb = [a*b for a,b in zip(X[:,2].flatten().tolist()[0],coeffs['smb'].values)]
+        fit_hml = [a*b for a,b in zip(X[:,3].flatten().tolist()[0],coeffs['hml'].values)]
     
 
     fitted = [a+b+c+d+e for a,b,c,d,e in zip(fit_const,fit_rmrf,fit_r_1,fit_smb,fit_hml)]
     
-    resids = [a-b for a,b, in zip(Y[start:end].flatten().tolist()[0],fitted)]
+    resids = [a-b for a,b, in zip(Y.flatten().tolist()[0],fitted)]
     
     mean_resid = np.mean(resids)
     centered_resids = [r-mean_resid for r in resids]
@@ -52,31 +53,25 @@ def bs_resample(resids):
 # X: full X matrix used for initial estimation 
 # resids: bootstrap sample of residuals from alternative model estimation
 # factor_model: string, either 'CAPM' or 'FF3'
-# start: row index of first time value for which we have coeff. estimates (integer)
-# end: row index of last time value for which we have coeff. estimates (integer)
-def gen_recursive(hypothesis,coeffs,X,resids,factor_model,start,end):
-    r_0 = X[start,-1] # lagged return value to serve as "seed" for DGP
-    
-    # not really sure how to deal with the DGP outside of the timeframe for which we have coeff. estimates...
-    # for now, let's just use the original outcome values...and maybe add some residual noise later? (Need to ask about this)
-    X_sub = X[start:end]
-    
+def gen_recursive(hypothesis,coeffs,X,resids,factor_model):
+    r_0 = X[0,-1] # lagged return value to serve as "seed" for DGP
+        
     # initialize empty list of new returns
-    r_star = [0]*len(X_sub)
+    r_star = [0]*len(X)
     
     # generate r* values
-    for row in range(len(X_sub)): 
+    for row in range(len(X)): 
         if row == 0:
             lagged_r = r_0
         else:
             lagged_r = r_star[row-1]
-        current_r = coeffs.loc[row,'const'] + (coeffs.loc[row,'rm_rf']*X_sub[row,1]) + (coeffs.loc[row,'r_1']*lagged_r) + resids[row]
+        current_r = coeffs.loc[row,'const'] + (coeffs.loc[row,'rm_rf']*X[row,1]) + (coeffs.loc[row,'r_1']*lagged_r) + resids[row]
         if factor_model == 'FF3':
-            current_r = current_r + (coeffs.loc[row,'smb']*X_sub[row,2]) + (coeffs.loc[row,'hml']*X_sub[row,3])
+            current_r = current_r + (coeffs.loc[row,'smb']*X[row,2]) + (coeffs.loc[row,'hml']*X[row,3])
         r_star[row] = current_r
     
     # replace lagged return values in X with r*
-    X_sub[1:,-1] = np.matrix(r_star[:len(r_star)-1]).T
+    X[1:,-1] = np.matrix(r_star[:len(r_star)-1]).T
     Y_star = np.matrix(r_star).T
     
     return [X,Y_star]
--- tvc_estimation.py
+++ tvc_estimation.py
@@ -16,38 +16,23 @@ import multiprocessing
 from clean_data import *
 from residual_bootstrap import *
 
-"""
-# Attempt at gauss kernel, the results makes no sense
-# Kernel weights don't sum to 1?
 
-def gauss(a):
-    return (1/math.sqrt(2*math.pi))*np.exp(-(a**2)/2)
-
-times = list(range(100))
-h = 0.1
-n = len(times)
-for t in times[1:99]:
-    timediffs = [(((i/n) - (t/n))/h) for i in times]
-    print(sum(timediffs))
-    testwts = [gauss(d)/h for d in timediffs]
-    print(sum(testwts))
-"""
-
-# symmetric uniform weights (until Gauss kernel is fixed)
-# t: target time, passed as implicit index starting at 0
-# h: bandwidth (proportion on [0,1])
-# times: list of t on [1...n]
-def unif(t,h,times):
-    interval = math.floor((h*len(times))/2)
-    wts = [0]*len(times)
-    wts[t - interval:t + interval + 1] = [1/len(wts[t - interval:t + interval + 1])]*len(wts[t - interval:t + interval + 1])
+# gaussian kernel weights
+# tau: target time value on [0,1]
+# times: set of all regularized times, from 0 to 1
+# h: bandwidth as proportion of total data
+def gauss(tau,times,h):
+    kernel_inputs = [(t-tau)/h for t in times]
+    kernel_outputs = [(1/math.sqrt(2*math.pi))*np.exp(-(i**2)/2) for i in kernel_inputs]
+    wts = [k/h for k in kernel_outputs]
     return wts
+
         
 # kernels to choose from. 
 # right now just uniform
 kernels = {
   
-        'uniform':unif
+        'gaussian':gauss
         
 }
 
@@ -57,13 +42,13 @@ kernels = {
 # Y: dependent variable matrix
 # target: target time on [1...n], passed as implicit index starting at 0
 # h: bandwidth (proportion on [0,1])
-def wls(X,Y,target,h,wt_fxn):
+def wls(X,Y,target,wt_fxn,h):
     print(target)
-    W = np.diag(kernels[wt_fxn](target,h,list(range(len(Y))))) # diagonal matrix of weights
+    tau = target/len(Y) # normalize target value on [0,1]
+    times = [i/len(Y) for i in list(range(len(Y)))]
+    W = np.diag(kernels[wt_fxn](tau,times,h)) # diagonal matrix of weights
     # B_hat = inv(X'WX) * (X'WY)
     B_hat = np.linalg.inv(X.T*W*X)*(X.T*W*Y) 
-    # get residuals
-    resids = Y - (X*B_hat)
     # return coefficient estimates and residuals
     return B_hat.flatten().tolist()[0]
 
@@ -80,50 +65,49 @@ asset = 'Telcm'
 X = gen_X(data,asset,'CAPM')
 Y = (np.matrix(data.loc[1:,asset].values).T)
 
-n = len(Y)
-h_test = 0.1
-start = math.floor((h*n)/2)
-end = n - start
-
-datelist = list(range(len(Y)))[start:end]
+datelist = list(range(len(Y)))
 
 # run for loop in parallel
 # with 8 cores and about 10 years of data, this runs in ~ 25 secs
 # --> 500 bs replications should take ~ 35 mins
-results = Parallel(n_jobs=numcores)(delayed(wls)(X=X,Y=Y,target=date,h=h_test,wt_fxn='uniform') for date in datelist)
+results = Parallel(n_jobs=numcores)(delayed(wls)(X=X,Y=Y,target=date,wt_fxn='gaussian',h=h_test) for date in datelist)
 
 coeffs = pd.DataFrame(results)
 coeffs.columns = ['const','rm_rf','r_1']
 
 # plot fitted values
-fitted = get_residuals('alternative',X,Y,coeffs,'CAPM',start,end)['fitted']
+fitted = get_residuals('alternative',X,Y,coeffs,'CAPM')['fitted']
 
-x = range(1,len(datelist_test)+1)
+x = range(len(datelist_test))
 
 fig, ax = plt.subplots()
 
 line1, = ax.plot(x[:100], fitted[:100].values, dashes = [5,5], linewidth=2,color='b',label='fitted')
-line2, = ax.plot(x[:100], Y[start:start+100], linewidth=2,color='r')
+line2, = ax.plot(x[:100], Y[:100], linewidth=2,color='r')
 
 plt.show()
 
 ###############################################################################
 
-centered_resids = get_residuals('alternative',X,Y,coeffs,'CAPM',start,end)['centered_resids']
+datelist = list(range(len(Y)))
 
-bs_resids = bs_resample(centered_resids.values)
+centered_resids = get_residuals('alternative',X,Y,coeffs,'CAPM')['centered_resids']
 
-recursive_test = gen_recursive('alternative',coeffs,X.copy(),bs_resids,'CAPM',start,end)
+const_bs = pd.DataFrame()
+rm_rf_bs = pd.DataFrame()
+r_1_bs = pd.DataFrame()
 
-X_recursive = recursive_test[0] # full X-matrix, with [start+1:end,'r_1'] replaced with recursively-generated outcomes
-Y_recursive = Y.copy()
-Y_recursive[start:end] = recursive_test[1] # full Y-matrix, with [start:end] replaced with recursively-generated outcomes
-
-x = range(len(Y[start:start+100]))
-
-fig, ax = plt.subplots()
-
-line1, = ax.plot(x, Y[start:start+100], linewidth=2,color='b')
-line2, = ax.plot(x, Y_recursive[start:start+100], dashes = [5,5], linewidth=2,color='r')
-
-plt.show()
+B = 5 # number of bootstrap replications
+for i in range(B):
+    print('bs iteration: ', i)
+    bs_resids = bs_resample(centered_resids.values)
+    X_cp = X.copy()
+    recursive = gen_recursive('alternative',coeffs,X_cp,bs_resids,'CAPM')
+    X_recur = recursive[0]
+    Y_recur = recursive[1]
+    bs_est = Parallel(n_jobs=numcores)(delayed(wls)(X=X_recur,Y=Y_recur,target=date,wt_fxn='gaussian',h=h_test) for date in datelist)
+    bs_est_mat = np.asmatrix(bs_est)
+    const_bs[i] = bs_est_mat[:,0].flatten().tolist()[0]
+    rm_rf_bs[i] = bs_est_mat[:,1].flatten().tolist()[0]
+    r_1_bs[i] = bs_est_mat[:,2].flatten().tolist()[0]
+    
